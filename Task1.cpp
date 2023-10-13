7c9db650e07338531b19d4ff736d606d3ece56bb
Samuel Wu <cernunnos1710@gmail.com>
2023-10-13 20:26:17 +0100

    some inline asm and fixed more comments

--- Task1.cpp
+++ Task1.cpp
@@ -1,6 +1,7 @@
 #include "Microbit.h"
 
 #define _DEBUG
+#define USE_INLINE_ASM
 
 /* Pin mapping from GPIO to edge connector pins */
 #define P0    2
@@ -13,7 +14,7 @@
 #define P14   1
 #define P15   13
 
-#define NUM_LEDS    8   // number of LEDs
+#define NUM_LEDS    8   // Number of LEDs
 
 /* GPIO addresses : base (0x50000000) + offset */
 #define GPIO_OUT        0x50000504
@@ -33,7 +34,7 @@
 #define GPIO_EVENT_CLEAR    0
 
 /* Bit patterns that turns out the required pins for LEDs from 0 to 14 */
-#define LED_BITS        0x82061E    // or 8521246 in decimal
+#define LED_BITS        0x82061E    // Or 8521246 in decimal
 
 /* Bit shift function */
 #define BIT_SHIFT(pin)  (uint32_t) (1 << pin)
@@ -50,6 +51,7 @@ const uint32_t LED_MASKS[] = {
     BIT_SHIFT(P0)
 };
 
+#ifndef USE_INLINE_ASM
 /********************* Helper functions *********************/
 
 /* Switches bits based on mask */
@@ -58,7 +60,7 @@ void switchBitsWithMask(volatile uint32_t mask)
     volatile uint32_t *out = (volatile uint32_t *) GPIO_OUT;
     volatile uint32_t *dir = (volatile uint32_t *) GPIO_DIR;
 
-    /* rewrite out and dir with mask value without OR */
+    /* Rewrite out and dir with mask value without OR */
     *out = mask;
     *dir = mask;
 }
@@ -66,9 +68,110 @@ void switchBitsWithMask(volatile uint32_t mask)
 /* Delays execution based on milliseconds*/
 void delay(int ms)
 {
-    volatile int count = ms * 9000; // rough number based on experiments
+    volatile int count = ms * 9000; // Rough number based on experiments
 
     while (count--);
+}
+
+/********************* Task functions *********************/
+
+void turnOn()
+{
+    switchBitsWithMask((uint32_t) LED_BITS);
+}
+
+void setLEDs(uint8_t value)
+{
+    uint8_t i;
+    volatile uint32_t mask = 0;
+
+    /* Switch on LED pin mask based on given value */
+    for (i = 0; i < NUM_LEDS; i++) {
+        if (value & BIT_SHIFT(i))
+            mask |= LED_MASKS[i];
+    }
+    switchBitsWithMask(mask);
+}
+
+void rollingCounter()
+{
+    uint8_t i = 0;
+
+    for(;;) {
+        setLEDs(i++);
+        delay(120); // Approx. delay of 118ms for 256 ops.
+    }
+}
+
+void knightRider()
+{
+    int8_t direction = 1;
+    uint8_t position = 0;
+
+    for (;;) {
+        setLEDs(1 << position);
+        delay(100);
+
+        /* Check boundary conditions and reverse if needed */
+        if ((position == (NUM_LEDS - 1) && direction == 1) ||
+                (position == 0 && direction == -1))
+            direction = -direction;
+
+        position += direction;
+    }
+}
+
+void countClicks()
+{
+    uint8_t i = 0;
+    uint32_t current_state = 0, last_state = 0;
+    volatile uint32_t *events_in = (volatile uint32_t *) GPIOTE_IN;
+    volatile uint32_t *task_config = (volatile uint32_t *) GPIOTE_CONFIG;
+
+    /* Configure the task for falling edge detection on P13 */
+    *task_config |= EVENT_MODE | PSEL_13 | FALLING_EDGE;
+
+    for (;;) {
+        current_state = *events_in;
+
+        if (current_state == GPIO_EVENT_DETECT &&
+                last_state == GPIO_EVENT_CLEAR) {
+            setLEDs(++i);
+            *events_in = GPIO_EVENT_CLEAR; // Clear input after detection
+        }
+        last_state = current_state;
+
+        delay(10);  // Debouncing delay prevents spurious transitions
+    }
+}
+
+#else
+/********************* Helper functions *********************/
+
+/* Switches bits based on mask */
+void switchBitsWithMask(volatile uint32_t mask)
+{
+    __asm__ volatile(
+        "str %[mask_val], [%[out]] \n\t"    // Store mask value to GPIO_OUT
+        "str %[mask_val], [%[dir]]"         // Store mask value to GPIO_DIR
+        : /* No outputs */
+        : [mask_val]"r" (mask), [out]"r" (GPIO_OUT), [dir]"r" (GPIO_DIR)
+    );
+}
+
+/* Delays execution based on milliseconds*/
+void delay(int ms)
+{
+    __asm__ volatile(
+        "mov r2, #0 \n\t"           // Initialize r2 to 0
+        "ldr r3, = 9000 \n\t"       // Load 9000 to r3
+        "mul r2, %[_ms], r3 \n\t"   // Multiply ms by 9000 to get count
+        "1: subs r2, r2, #1 \n\t"   // Subtract 1 from count
+        "bne 1b"                    // If count is not 0, branch to 1
+        :
+        : [_ms]"r" (ms)
+        : "cc", "r2", "r3"
+    );
 }
 
 /********************* Task functions *********************/
@@ -142,6 +245,7 @@ void countClicks()
         delay(10);  // debouncing delay prevents spurious transitions
     }
 }
+#endif
 
 #ifdef _DEBUG
 #define TEST_UINT 81
@@ -150,9 +254,8 @@ int main()
 {
     // turnOn();
     // setLEDs((uint8_t)TEST_UINT);
-    // rollingCounter();
+    rollingCounter();
     // knightRider();
-    countClicks();
+    // countClicks();
 }
-
 #endif
