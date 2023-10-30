fd6828ea7f72775275f45e0b86ab2036412c2deb
Samuel Wu <cernunnos1710@gmail.com>
2023-10-30 21:21:31 +0000

    need to fix update interval

--- Task2.cpp
+++ Task2.cpp
@@ -10,9 +10,10 @@
 
 #define PRESCALER_VALUE 8
 
-#define LED_ROWS 5
-#define LED_COLS 5
-#define NUM_DIGITS 10
+#define LED_ROWS    5
+#define LED_COLS    5
+#define NUM_DIGITS  10
+#define NUM_ITER    25
 
 /**
  * Rough approximation of number of loop iterations to approximate 1 ms.
@@ -38,105 +39,31 @@ enum Columns {
     COL5 = 30
 };
 
-enum LEDState {
-    LED_STATE_1,
-    LED_STATE_2,
-    LED_STATE_3,
-    LED_STATE_CLEAR
-};
+const Rows rows[] = {ROW1, ROW2, ROW3, ROW4, ROW5};
+const Columns cols[] = {COL1, COL2, COL3, COL4, COL5};
 
 const uint8_t led_digits[NUM_DIGITS][LED_ROWS] = {
-    {   /* 0 */
-        0b01110,
-        0b01010,
-        0b01010,
-        0b01010,
-        0b01110
-    },
-    {   /* 1 */
-        0b00100,
-        0b01100,
-        0b00100,
-        0b00100,
-        0b01110
-    },
-    {   /* 2 */
-        0b01110,
-        0b00010,
-        0b01110,
-        0b01000,
-        0b01110
-    },
-    {   /* 3 */
-        0b01110,
-        0b00010,
-        0b01110,
-        0b00010,
-        0b01110
-    },
-    {   /* 4 */
-        0b01010,
-        0b01010,
-        0b01110,
-        0b00010,
-        0b00010
-    },
-    {   /* 5 */
-        0b01110,
-        0b01000,
-        0b01110,
-        0b00010,
-        0b01110
-    },
-    {   /* 6 */
-        0b01110,
-        0b01000,
-        0b01110,
-        0b01010,
-        0b01110
-    },
-    {   /* 7 */
-        0b01110,
-        0b01010,
-        0b00010,
-        0b00010,
-        0b00010
-    },
-    {   /* 8 */
-        0b01110,
-        0b01010,
-        0b01110,
-        0b01010,
-        0b01110
-    },
-    {   /* 9 */
-        0b01110,
-        0b01010,
-        0b01110,
-        0b00010,
-        0b01110
-    }
+    { 0b01110, 0b01010, 0b01010, 0b01010, 0b01110 },
+    { 0b00100, 0b01100, 0b00100, 0b00100, 0b01110 },
+    { 0b01110, 0b00010, 0b01110, 0b01000, 0b01110 },
+    { 0b01110, 0b00010, 0b01110, 0b00010, 0b01110 },
+    { 0b01010, 0b01010, 0b01110, 0b00010, 0b00010 },
+    { 0b01110, 0b01000, 0b01110, 0b00010, 0b01110 },
+    { 0b01110, 0b01000, 0b01110, 0b01010, 0b01110 },
+    { 0b01110, 0b01010, 0b00010, 0b00010, 0b00010 },
+    { 0b01110, 0b01010, 0b01110, 0b01010, 0b01110 },
+    { 0b01110, 0b01010, 0b01110, 0b00010, 0b01110 }
 };
 
-uint32_t convertMsToTicks(float delay_ms);
-const uint32_t interval = convertMsToTicks(5);
+const bool smiley_face[LED_ROWS][LED_COLS] = {
+    {0, 0, 0, 0, 0},
+    {0, 1, 0, 1, 0},
+    {0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 1},
+    {0, 1, 1, 1, 0}
+};
 
-volatile LEDState current_state = LED_STATE_1;
-
-/********************* Helper functions *********************/
-
-void setLED(Rows row, Columns col)
-{
-    ((col == COL4) ? NRF_P1 : NRF_P0)->DIR |= BIT_SHIFT(col);
-    NRF_P0->DIR |= BIT_SHIFT(row);
-    NRF_P0->OUT = BIT_SHIFT(row);
-}
-
-void clearLEDs()
-{
-    NRF_P1->DIR = 0;
-    NRF_P0->DIR = 0;
-}
+/* ##########################  Timer functions  #################################### */
 
 uint32_t convertMsToTicks(float delay_ms)
 {
@@ -146,9 +73,9 @@ uint32_t convertMsToTicks(float delay_ms)
     return (uint32_t)(ticks_per_ms * delay_ms);
 }
 
-void startTimer()
+void initRegularTimer()
 {
-    NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_08Bit;
+    NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_24Bit;
     NRF_TIMER1->PRESCALER = PRESCALER_VALUE;
     NRF_TIMER1->TASKS_START = 1;
 }
@@ -186,73 +113,66 @@ void delay(uint32_t delay_ms)
     while (count--);
 }
 
-void setLEDState(LEDState state) {
-    clearLEDs();
-    switch (state) {
-        case LED_STATE_1:
-            setLED(ROW2, COL2);
-            setLED(ROW2, COL4);
-            break;
-        case LED_STATE_2:
-            setLED(ROW4, COL1);
-            setLED(ROW4, COL5);
-            break;
-        case LED_STATE_3:
-            setLED(ROW5, COL2);
-            setLED(ROW5, COL3);
-            setLED(ROW5, COL4);
-            break;
-        case LED_STATE_CLEAR:
-            // LEDs already cleared at the start
-            break;
-    }
+/* ##########################  LED functions  #################################### */
+
+void setLED(Rows row, Columns col)
+{
+    ((col == COL4) ? NRF_P1 : NRF_P0)->DIR |= BIT_SHIFT(col);
+    NRF_P0->DIR |= BIT_SHIFT(row);
+    NRF_P0->OUT = BIT_SHIFT(row);
 }
 
-LEDState updateState(LEDState state) {
-    return static_cast<LEDState>((state + 1) % (LED_STATE_CLEAR + 1));
+void clearLEDs()
+{
+    NRF_P1->DIR = 0;
+    NRF_P0->DIR = 0;
+}
+
+void updateLEDs(const bool led_states[LED_ROWS][LED_COLS])
+{
+    int row, col;
+    volatile int iter;
+
+    for (iter = 0; iter < NUM_ITER; iter++)
+        for (row = 0; row < LED_ROWS; row++) {
+            for (col = 0; col < LED_COLS; col++)
+                if (led_states[row][col])
+                    setLED(rows[row], cols[col]);
+            clearLEDs();
+        }
+}
+
+void drawSmile()
+{
+    clearLEDs();
+    updateLEDs(smiley_face);
+}
+
+/* ##########################  Interrupt functions  #################################### */
+
+void initInterruptTimer(uint32_t interval)
+{
+    NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_24Bit;
+    NRF_TIMER1->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Msk;
+    NRF_TIMER1->PRESCALER = PRESCALER_VALUE;
+    NRF_TIMER1->CC[0] = interval; // Set interval for compare match
+}
+
+void startInterruptTimer()
+{
+    NRF_TIMER1->TASKS_START = 1;
 }
 
 void TIMER1_IRQHandler()
 {
     if (NRF_TIMER1->EVENTS_COMPARE[0] != 0) {
         NRF_TIMER1->EVENTS_COMPARE[0] = 0;  // Clear match event
-        setLEDState(current_state);
-        current_state = updateState(current_state);
+        drawSmile();
     }
 }
 
-/********************* Task functions *********************/
-
-void beHappy()
+void configureInterrupt()
 {
-    while(1) {
-        setLEDState(current_state);
-        delay(5);
-        current_state = updateState(current_state);
-    }
-}
-
-void beVeryHappy()
-{
-    startTimer();
-    uint32_t next_time = captureTime() + interval;
-
-    while(1) {
-        setLEDState(current_state);
-        delayUntil(next_time);
-        next_time += interval;
-        current_state = updateState(current_state);
-    }
-}
-
-void beHappyAndFree()
-{
-    NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_08Bit;
-    NRF_TIMER1->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Msk;
-
-    // Load the initial value to the timer
-    NRF_TIMER1->CC[0] = interval; // 50Hz update rate
-
     // Enable interrupt on compare match
     NRF_TIMER1->INTENSET = TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos;
 
@@ -260,47 +180,27 @@ void beHappyAndFree()
     NVIC_SetVector(TIMER1_IRQn, (uint32_t)TIMER1_IRQHandler);
     NVIC_EnableIRQ(TIMER1_IRQn);
     NVIC_SetPriority(TIMER1_IRQn, 0);   // Set priority to 0
-
-    // Start the timer
-    NRF_TIMER1->TASKS_START = 1;
 }
 
-const Rows rows[] = {ROW1, ROW2, ROW3, ROW4, ROW5};
-const Columns cols[] = {COL1, COL2, COL3, COL4, COL5};
+/* ##########################  String manipulation functions  #################################### */
 
-void showNumber(int n)
+void reverse(char *str, int length)
 {
-    if (n < 0 || n >= NUM_DIGITS) return;  // Out of range
-
-    while(1) {
-        for (int row = 0; row < LED_ROWS; row++) {
-            uint8_t row_data = led_digits[n][row];
-            for (int col = 0; col < LED_COLS; col++) {
-                if (row_data & (1 << (LED_COLS - 1 - col))) {
-                    setLED(rows[row], cols[col]);
-                }
-            }
-            delay(1);  // Add a short delay to make the LEDs visible
-            clearLEDs();
-        }
-    }
-}
-
-void reverse(char *str, int length) {
-    int start = 0, end = length - 1;
+    int start = 0;
+    int end = length - 1;
     char temp;
 
-    while (start < end) {
+    for (; start < end; start++, end--) {
         temp = str[start];
         str[start] = str[end];
         str[end] = temp;
-        start++;
-        end--;
     }
 }
 
-void intToStr(int num, char *str) {
+void intToStr(char *str, int num)
+{
     int i = 0;
+
     if (num == 0) {
         str[i++] = '0';
     } else {
@@ -310,66 +210,88 @@ void intToStr(int num, char *str) {
         }
         while (num > 0) {
             str[i++] = (num % 10) + '0';
-            num = num / 10;
+            num /= 10;
         }
-        if (str[0] == '-') {
+        if (str[0] == '-')
             reverse(str + 1, i - 1);
-        } else {
+        else
             reverse(str, i);
-        }
     }
     str[i] = '\0';
 }
 
-int strLength(char *str) {
+int strLength(char *str)
+{
     int length = 0;
-    while (str[length] != '\0') {
-        length++;
-    }
+
+    while (str[length++] != '\0');
     return length;
 }
 
-void updateLEDs(const bool led_states[LED_ROWS][LED_COLS]) {
-    for (volatile int i = 0; i < 25; i++) {
-        for (int row = 0; row < LED_ROWS; ++row) {
-            for (int col = 0; col < LED_COLS; ++col) {
-                if (led_states[row][col]) {
+void* my_memset(void* ptr, int value, size_t num) {
+    unsigned char* p = static_cast<unsigned char*>(ptr);
+
+    while (num--)
+        *p++ = static_cast<unsigned char>(value);
+    return ptr;
+}
+
+/* ##########################  Number scrolling functions  #################################### */
+
+void showSingleNumber(int n)
+{
+    if (n < 0 || n >= NUM_DIGITS) return;  // Out of range
+    int row, col;
+    uint8_t row_data;
+
+    while(1) {
+        for (row = 0; row < LED_ROWS; row++) {
+            row_data = led_digits[n][row];
+
+            for (col = 0; col < LED_COLS; col++)
+                if (row_data & (1 << (LED_COLS - 1 - col)))
                     setLED(rows[row], cols[col]);
-                }
-            }
-            delay(1);
+            delay(1);  // Add a short delay to make the LEDs visible
             clearLEDs();
         }
     }
 }
 
-void showPartialDigit(int n, int start_col, bool led_states[LED_ROWS][LED_COLS]) {
-    if (n < 0 || n >= NUM_DIGITS || start_col > LED_COLS) return;
+void showPartialDigit(int n, int start_col, bool led_states[LED_ROWS][LED_COLS])
+{
+    if (start_col > LED_COLS) return;
+    int row, col, led_col;
+    uint8_t row_data;
 
-    for (int row = 0; row < LED_ROWS; ++row) {
-        uint8_t row_data = led_digits[n][row];
-        for (int col = 0; col < LED_COLS; ++col) {
-            int led_col = col + start_col;
+    for (row = 0; row < LED_ROWS; ++row) {
+        row_data = led_digits[n][row];
+
+        for (col = 0; col < LED_COLS; ++col) {
+            led_col = col + start_col;
+
             if (led_col < 0 || led_col >= LED_COLS) continue;
             led_states[row][led_col] = (row_data & (1 << (LED_COLS - 1 - col))) != 0;
         }
     }
 }
 
-void showScrollingNumber(int n) {
+void showScrollingNumber(int n)
+{
     char num_str[12];  // Enough space for 32-bit integer, sign, and null terminator
-    intToStr(n, num_str);
+    intToStr(num_str, n);
     int length = strLength(num_str);
     bool led_states[LED_ROWS][LED_COLS] = { false };
+    int pos, i;
+    int digit_start_col;
 
-    for (int pos = length * LED_COLS; pos >= -LED_COLS; --pos) {
-        memset(led_states, 0, sizeof(led_states));  // Clear the buffer
-        for (int i = 0; i < length; ++i) {
-            int digit_start_col = pos - (length - 1 - i) * LED_COLS; // Reverse
-            if (num_str[i] >= '0' && num_str[i] <= '9') {
-                int digit = num_str[i] - '0';
-                showPartialDigit(digit, digit_start_col, led_states);
-            }
+    for (pos = length * LED_COLS; pos >= -LED_COLS; --pos) {
+        my_memset(led_states, 0, sizeof(led_states));  // Clear the buffer
+
+        for (i = 0; i < length; ++i) {
+            digit_start_col = pos - (length - 1 - i) * LED_COLS; // Reverse
+
+            if (num_str[i] >= '0' && num_str[i] <= '9')
+                showPartialDigit(num_str[i] - '0', digit_start_col, led_states);
         }
         updateLEDs(led_states);
         delay(3);  // Adjust delay as needed
@@ -377,18 +299,55 @@ void showScrollingNumber(int n) {
     }
 }
 
+/* ##########################  Task functions  #################################### */
 
+void beHappy()
+{
+    uint32_t interval = convertMsToTicks(5);
+
+    while(1) {
+        drawSmile();
+        delay(interval);
+    }
+}
+
+void beVeryHappy()
+{
+    uint32_t interval = convertMsToTicks(5);
+    initRegularTimer();
+    uint32_t next_time = captureTime() + interval;
+
+    while(1) {
+        drawSmile();
+        delayUntil(next_time);
+        next_time += interval;
+    }
+}
+void beHappyAndFree()
+{
+    initInterruptTimer(convertMsToTicks(60));
+    configureInterrupt();
+    startInterruptTimer();
+}
+
+void showNumber(int n)
+{
+    if (n < 0) return;  // Out of range
+
+    if (n >= 10)
+        showScrollingNumber(n);
+    else
+        showSingleNumber(n);
+}
 
 #ifdef _DEBUG
-#define TEST_UINT 1
+#define TEST_UINT 239512897
 
 int main()
 {
     // beHappy();
     // beVeryHappy();
-    // beHappyAndFree();
+    beHappyAndFree();
     // showNumber(TEST_UINT);
-    for (int i = 0; i < 100; i++)
-        showScrollingNumber(i);
 }
 #endif
