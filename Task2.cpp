0292dd171966b7fc739188a8ec0fa737ccf07a22
Samuel Wu <cernunnos1710@gmail.com>
2023-10-31 11:24:43 +0000

    refactoring

--- Task2.cpp
+++ Task2.cpp
@@ -13,8 +13,12 @@
 #define LED_ROWS    5
 #define LED_COLS    5
 #define NUM_DIGITS  10
-#define NUM_ITER    25
+#define BUF_LEN     12
 
+#define MS_TO_10HZ      4
+#define SCROLL_SPEED    50
+
+#define BIN_DIGIT(col)  (1 << (LED_COLS - 1 - col))
 /**
  * Rough approximation of number of loop iterations to approximate 1 ms.
  *
@@ -35,7 +39,7 @@ enum Columns {
     COL1 = 28,
     COL2 = 11,
     COL3 = 31,
-    COL4 = 5,   // NOTE: This is in P1 (srsly, why?)
+    COL4 = 5,   // NOTE: COL4 is in P1
     COL5 = 30
 };
 
@@ -66,60 +70,11 @@ const bool smiley_face[LED_ROWS][LED_COLS] = {
 volatile int current_row = 0;
 volatile bool led_buffer[LED_ROWS][LED_COLS] = { false };
 
-/* ####################################  Timer functions  #################################### */
-
-uint32_t convertMsToTicks(float delay_ms)
-{
-    float fTIMER = BASE_FREQ_16M / BIT_SHIFT(PRESCALER_VALUE);
-    float ticks_per_ms = fTIMER / MS_PER_SECOND;
-
-    return (uint32_t)(ticks_per_ms * delay_ms);
-}
-
-void initRegularTimer()
-{
-    NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_24Bit;
-    NRF_TIMER1->PRESCALER = PRESCALER_VALUE;
-    NRF_TIMER1->TASKS_START = 1;
-}
-
-uint32_t captureTime()
-{
-    NRF_TIMER1->TASKS_CAPTURE[0] = 1;
-    NRF_TIMER1->TASKS_CAPTURE[0] = 0;   // Stops repeated captures
-    return NRF_TIMER1->CC[0];
-}
-
-/**
- * Hardware-based delay waits until timer reaches specified absolute time.
- *
- * This status-driven control mechanism ensures higher accuracy by preventing
- * cumulative drift. The system pauses, on average, for the provided interval.
- */
-void delayUntil(uint32_t next_time)
-{
-    NRF_TIMER1->CC[0] = next_time;
-    while (!(NRF_TIMER1->EVENTS_COMPARE[0]));
-    NRF_TIMER1->EVENTS_COMPARE[0] = 0;
-}
-
-/**
- * Software delay that pauses execution for a given interval in milliseconds.
- *
- * Utilizes a simple busy-wait loop. Due to local and cumulative drifts, the
- * actual (software) delay might be slightly longer than the specified interval.
- */
-void delay(uint32_t delay_ms)
-{
-    volatile int count = delay_ms * DELAY_1MS_ITERATIONS;
-
-    while (count--);
-}
-
 /* ####################################  LED functions  #################################### */
 
 void setLED(Rows row, Columns col)
 {
+    // We do this check because COL4 is in P1 (srsly, why?)
     ((col == COL4) ? NRF_P1 : NRF_P0)->DIR |= BIT_SHIFT(col);
     NRF_P0->DIR |= BIT_SHIFT(row);
     NRF_P0->OUT = BIT_SHIFT(row);
@@ -149,15 +104,66 @@ void updateLEDBuffer(const bool led_states[LED_ROWS][LED_COLS])
             led_buffer[row][col] = led_states[row][col];
 }
 
-void updateBufferWithDigit(int digit) {
-    bool led_states[LED_ROWS][LED_COLS] = { false };
+void updateBufferWithDigit(int digit)
+{
     int row, col;
+    bool led_states[LED_ROWS][LED_COLS] = { false };
 
-    for (row = 0; row < LED_ROWS; ++row)
-        for (col = 0; col < LED_COLS; ++col)
-            if (led_digits[digit][row] & (1 << (LED_COLS - 1 - col)))
+    for (row = 0; row < LED_ROWS; row++)
+        for (col = 0; col < LED_COLS; col++)
+            if (led_digits[digit][row] & BIN_DIGIT(col))
                 led_states[row][col] = true;
     updateLEDBuffer(led_states);
+}
+
+/* ####################################  Timer functions  #################################### */
+
+uint32_t convertMsToTicks(float delay_ms)
+{
+    float fTIMER = BASE_FREQ_16M / BIT_SHIFT(PRESCALER_VALUE);
+    float ticks_per_ms = fTIMER / MS_PER_SECOND;
+
+    return ticks_per_ms * delay_ms;
+}
+
+void initRegularTimer()
+{
+    NRF_TIMER0->BITMODE = TIMER_BITMODE_BITMODE_24Bit;
+    NRF_TIMER0->PRESCALER = PRESCALER_VALUE;
+    NRF_TIMER0->TASKS_START = 1;
+}
+
+uint32_t captureTime()
+{
+    NRF_TIMER0->TASKS_CAPTURE[0] = 1;
+    NRF_TIMER0->TASKS_CAPTURE[0] = 0;   // Stops repeated captures
+    return NRF_TIMER0->CC[0];
+}
+
+/**
+ * Hardware-based delay waits until timer reaches specified absolute time.
+ *
+ * This status-driven control mechanism ensures higher accuracy by preventing
+ * cumulative drift. The system pauses, on average, for the provided interval.
+ */
+void delayUntil(uint32_t next_time)
+{
+    NRF_TIMER0->CC[0] = next_time;
+    while (!(NRF_TIMER0->EVENTS_COMPARE[0]));
+    NRF_TIMER0->EVENTS_COMPARE[0] = 0;
+}
+
+/**
+ * Software delay that pauses execution for a given interval in milliseconds.
+ *
+ * Utilizes a simple busy-wait loop. Due to local and cumulative drifts, the
+ * actual (software) delay might be slightly longer than the specified interval.
+ */
+void delay(uint32_t delay_ms)
+{
+    volatile int count = delay_ms * DELAY_1MS_ITERATIONS;
+
+    while (count--);
 }
 
 /* ####################################  Interrupt functions  #################################### */
@@ -179,7 +185,6 @@ void TIMER1_IRQHandler()
 {
     if (NRF_TIMER1->EVENTS_COMPARE[0] != 0) {
         NRF_TIMER1->EVENTS_COMPARE[0] = 0;  // Clear match event
-
         clearLEDs();
         updateLEDRow(current_row);  // Update current row
         current_row = (current_row + 1) % LED_ROWS;  // Move to the next row
@@ -190,8 +195,6 @@ void configureInterrupt()
 {
     // Enable interrupt on compare match
     NRF_TIMER1->INTENSET = TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos;
-
-    // Enable interrupt in the NVIC
     NVIC_SetVector(TIMER1_IRQn, (uint32_t)TIMER1_IRQHandler);
     NVIC_EnableIRQ(TIMER1_IRQn);
     NVIC_SetPriority(TIMER1_IRQn, 0);
@@ -227,7 +230,7 @@ void intToStr(char *str, int num)
     str[i] = '\0';
 }
 
-int strLength(char *str)
+int my_strlen(char *str)
 {
     int length = 0;
 
@@ -249,55 +252,73 @@ void* my_memset(void* ptr, int value, size_t num)
 void showSingleNumber(int n)
 {
     updateBufferWithDigit(n);
-    initInterruptTimer(convertMsToTicks(4));
+    initInterruptTimer(convertMsToTicks(MS_TO_10HZ));
     configureInterrupt();
     startInterruptTimer();
 }
 
-void showPartialDigit(int n, int start_col,
-                        bool led_states[LED_ROWS][LED_COLS])
+/**
+ * Display a digit on the LED matrix starting at a specific column.
+ *
+ * The digit is displayed using the LED states from the `led_digits` array.
+ */
+void showPartialDigit(int n, int start_col, bool led_states[LED_ROWS][LED_COLS])
 {
     if (start_col > LED_COLS) return;
+
     int row, col, led_col;
     uint8_t row_data;
 
-    for (row = 0; row < LED_ROWS; ++row) {
+    for (row = 0; row < LED_ROWS; row++) {
+        // Get the binary representation of the digit for the current row
         row_data = led_digits[n][row];
 
-        for (col = 0; col < LED_COLS; ++col) {
+        for (col = 0; col < LED_COLS; col++) {
             led_col = col + start_col;
 
-            if (led_col < 0 || led_col >= LED_COLS)
-                continue;
-            led_states[row][led_col] =
-                (row_data & (1 << (LED_COLS - 1 - col))) != 0;
+            // Skip columns that are outside the bounds of the LED matrix
+            if (led_col < 0 || led_col >= LED_COLS) continue;
+
+            // Set the LED state based on the binary representation of the digit
+            led_states[row][led_col] = (row_data & BIN_DIGIT(col));
         }
     }
     updateLEDBuffer(led_states);
 }
 
+/**
+ * Convert a number to a string and display it as a scrolling text on the LED matrix.
+ */
 void showScrollingNumber(int n)
 {
-    char num_str[12];  // Enough space for 32-bit integer
-    intToStr(num_str, n);
-    int length = strLength(num_str);
-    bool led_states[LED_ROWS][LED_COLS] = { false };
     int pos, len;
     int digit_start_col;
+    const uint32_t interval = convertMsToTicks(SCROLL_SPEED);
+    bool led_states[LED_ROWS][LED_COLS] = { false };
 
-    initInterruptTimer(4);
+    char num_str[BUF_LEN];  // Buffer to store string representation of the number
+    intToStr(num_str, n);
+    int length = my_strlen(num_str);
+
+    initInterruptTimer(convertMsToTicks(MS_TO_10HZ));
     configureInterrupt();
     startInterruptTimer();
 
-    for (pos = length * LED_COLS; pos >= -LED_COLS; pos--) {
-        my_memset(led_states, 0, sizeof(led_states));  // Clear the buffer
-        for (len = 0; len < length; len++) {
-            digit_start_col = pos - (length - 1 - len) * LED_COLS; // Reverse
+    initRegularTimer();
+    uint32_t next_time = captureTime() + interval;
 
+    for (pos = length * LED_COLS; pos >= -LED_COLS; pos--) {
+        my_memset(led_states, 0, sizeof(led_states));
+        for (len = 0; len < length; len++) {
+            // Calculate the starting column of the current digit on the LED matrix
+            digit_start_col = pos - (length - 1 - len) * LED_COLS;
+
+            // If the character is a digit, display it on the LED matrix
             if (num_str[len] >= '0' && num_str[len] <= '9')
                 showPartialDigit(num_str[len] - '0', digit_start_col, led_states);
         }
-        delay(100);
+        delayUntil(next_time);
+        next_time += interval;
     }
 }
 
@@ -312,7 +333,7 @@ void beHappy()
             for (col = 0; col < LED_COLS; col++)
                 if (smiley_face[row][col])
                     setLED(rows[row], cols[col]);
-            delay(convertMsToTicks(5));
+            delay(convertMsToTicks(MS_TO_10HZ));
             clearLEDs();
         }
     }
@@ -321,7 +342,7 @@ void beHappy()
 void beVeryHappy()
 {
     int row, col;
-    const uint32_t interval = convertMsToTicks(5);
+    const uint32_t interval = convertMsToTicks(MS_TO_10HZ);
     initRegularTimer();
     uint32_t next_time = captureTime() + interval;
 
@@ -339,7 +360,7 @@ void beVeryHappy()
 void beHappyAndFree()
 {
     updateLEDBuffer(smiley_face);
-    initInterruptTimer(convertMsToTicks(5));
+    initInterruptTimer(convertMsToTicks(MS_TO_10HZ));
     configureInterrupt();
     startInterruptTimer();
 }
@@ -355,13 +376,14 @@ void showNumber(int n)
 }
 
 #ifdef _DEBUG
-#define TEST_UINT 239512897
+#define TEST_UINT 1817956776
 
 int main()
 {
     // beHappy();
     // beVeryHappy();
     // beHappyAndFree();
-    showNumber(TEST_UINT);
+    for (int i = 1; i < 200; i++)
+        showScrollingNumber(TEST_UINT);
 }
 #endif
