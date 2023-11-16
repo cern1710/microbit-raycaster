df27e25bf257f46d1fc2b428910eb3a1b73bc789
Samuel Wu <cernunnos1710@gmail.com>
2023-11-16 21:29:17 +0000

    delay(1) now

--- Task3.cpp
+++ Task3.cpp
@@ -6,13 +6,8 @@
 #define SCL_PIN 8
 #define SDA_PIN 16
 
-#define MMIO32(addr)    (*(volatile uint32_t *)(addr))
-
 #define SERIAL_TX_PIN   6
-#define CLOCK_SPEED     64000000
-#define TARGET_BAUD     115200
 
-#define TWI_FREQ_100_KBPS  0x01980000
 #define TWI_FREQ_250_KBPS  0x04000000
 
 #define ACCEL_I2C_ADDRESS   0x19
@@ -26,8 +21,6 @@
 
 #define TWI_DISABLE     0
 #define TWI_ENABLE      5
-
-void bitBangSerial(char *str, int delayCount);
 
 /**
  * Rough approximation of number of loop iterations to approximate 1 ms.
@@ -55,9 +48,9 @@ void resetI2CFlags()
     NRF_TWI0->ERRORSRC = 0xFFFFFFFF;
 }
 
-void delay(int delayCount)
+void delay(int delay_count)
 {
-    volatile int count = delayCount; // approx 75 to 81
+    volatile int count = delay_count; // approx 75 to 81
 
     while (count--);
 }
@@ -69,23 +62,13 @@ void delayMilliseconds(uint32_t delay_ms)
     while (count--);
 }
 
-void transmitAndDelay(bool level, int delayCount)
+void transmitAndDelay(bool level, int delay_count)
 {
     if (level)
         NRF_P0->OUTSET = (1UL << SERIAL_TX_PIN);
     else
         NRF_P0->OUTCLR = (1UL << SERIAL_TX_PIN);
-    delay(delayCount);
-}
-
-void sendByte(char byte, int delayCount)
-{
-    transmitAndDelay(false, delayCount);
-
-    for (int i = 0; i < 8; i++)
-        transmitAndDelay((byte >> i) & 0x01, delayCount);
-
-    transmitAndDelay(true, delayCount);
+    delay(delay_count);
 }
 
 /* ####################################  String manipulation functions  #################################### */
@@ -112,6 +95,18 @@ void myStrcat(char *dst, const char *src)
     *dst = '\0';
 }
 
+/* String reversal function */
+void reverse(char *str, int length)
+{
+    char temp;
+
+    for (int start = 0, end = length - 1; start < end; start++, end--) {
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+    }
+}
+
 void intToStr(int num, char *str)
 {
     int i = 0;
@@ -131,13 +126,7 @@ void intToStr(int num, char *str)
 
         if (isNegative)
             str[i++] = '-';
-
-        // Reverse the string in place
-        for (int start = 0, end = i - 1; start < end; start++, end--) {
-            char temp = str[start];
-            str[start] = str[end];
-            str[end] = temp;
-        }
+        reverse(str, i);
     }
     str[i] = '\0'; // Null-terminate the string
 }
@@ -155,28 +144,17 @@ void configureI2C()
     NRF_TWI0->SHORTS = 1;   // Suspend the peripheral after every byte
 }
 
-uint8_t readAccelerometerData(uint32_t axisRegister)
+uint8_t readAccelerometerData(uint32_t read_register)
 {
     uint8_t result = 0;
 
+    delay(1);
+
     NRF_TWI0->ENABLE = TWI_DISABLE;
-
-    // Configure SCL and SDA pin modes for I2C
-    NRF_P0->PIN_CNF[SCL_PIN] = 0x00000600;
-    NRF_P0->PIN_CNF[SDA_PIN] = 0x00000600;
-
-    NRF_TWI0->PSEL.SCL = SCL_PIN;
-    NRF_TWI0->PSEL.SDA = SDA_PIN;
-
-    NRF_TWI0->ADDRESS = ACCEL_I2C_ADDRESS;
-    NRF_TWI0->FREQUENCY = TWI_FREQ_250_KBPS;
-    NRF_TWI0->SHORTS = 1;   // Suspend the peripheral after every byte
-
-    // Reset I2C flags
     resetI2CFlags();
 
     NRF_TWI0->ENABLE = TWI_ENABLE;
-    NRF_TWI0->TXD = axisRegister;   // Desired axis register in accelerometer
+    NRF_TWI0->TXD = read_register;  // Desired axis register in accelerometer
     NRF_TWI0->TASKS_STARTTX = 1;    // Start an I2C write transaction
 
     // Wait for data to be sent
@@ -191,8 +169,7 @@ uint8_t readAccelerometerData(uint32_t axisRegister)
     while (NRF_TWI0->EVENTS_RXDREADY == 0);
     NRF_TWI0->EVENTS_RXDREADY = 0;
 
-    // Store the byte that has been received
-    result = NRF_TWI0->RXD;
+    result = NRF_TWI0->RXD;     // Store the byte that has been received
 
     // Finish the transaction
     NRF_TWI0->TASKS_RESUME = 1;
@@ -201,15 +178,13 @@ uint8_t readAccelerometerData(uint32_t axisRegister)
     return result;
 }
 
-void writeAccelerometerData(uint32_t axisRegister, uint32_t byte)
+void writeAccelerometerData(uint32_t write_register, uint32_t byte)
 {
     NRF_TWI0->ENABLE = TWI_DISABLE;
-
-    // Reset I2C flags
     resetI2CFlags();
 
     NRF_TWI0->ENABLE = TWI_ENABLE;
-    NRF_TWI0->TXD = axisRegister;   // Desired axis register in accelerometer
+    NRF_TWI0->TXD = write_register; // Desired axis register in accelerometer
     NRF_TWI0->TASKS_STARTTX = 1;    // Start an I2C write transaction
 
     while (NRF_TWI0->EVENTS_TXDSENT == 0);
@@ -231,55 +206,63 @@ void writeAccelerometerData(uint32_t axisRegister, uint32_t byte)
     NRF_TWI0->TASKS_STOP = 1;
 }
 
-void bitBangSerial(char *str, int delayCount)
+void configureAccelerometer()
 {
+    writeAccelerometerData(0x20, 0x97);
+    writeAccelerometerData(0x23, 0x08);
+}
+
+void sendByte(char byte, int delay_count)
+{
+    transmitAndDelay(false, delay_count);
+    for (int i = 0; i < 8; i++)
+        transmitAndDelay((byte >> i) & 0x01, delay_count);
+    transmitAndDelay(true, delay_count);
+}
+
+void bitBangSerial(char *str, int delay_count)
+{
+    initGPIO();
+
     while (*str)
-        sendByte(*str++, delayCount);
+        sendByte(*str++, delay_count);
 }
 
 void showAccelerometerSample()
 {
-    writeAccelerometerData(0x20, 0x67);
-    writeAccelerometerData(0x23, 8);
-    char output[50];
-    char numStr[10];
+    configureI2C();
+    configureAccelerometer();
+
+    char output[35];
+    char numStr[6];
     int16_t x, x1, x2;
     int16_t y, y1, y2;
     int16_t z, z1, z2;
 
     while (1) {
         x1 = readAccelerometerData(X_HIGH_AXIS_REGISTER) << 8;
-        delayMilliseconds(1);
+        y1 = readAccelerometerData(Y_HIGH_AXIS_REGISTER) << 8;
+        z1 = readAccelerometerData(Z_HIGH_AXIS_REGISTER) << 8;
         x2 = readAccelerometerData(X_LOW_AXIS_REGISTER);
+        y2 = readAccelerometerData(Y_LOW_AXIS_REGISTER);
+        z2 = readAccelerometerData(Z_LOW_AXIS_REGISTER);
+
         x = (int16_t)((x1 | x2) >> 5);
-        delayMilliseconds(1);
+        y = (int16_t)((y1 | y2) >> 5);
+        z = (int16_t)((z1 | z2) >> 5);
+
         intToStr(x, numStr);
         myStrcpy(output, "[X: ");
         myStrcat(output, numStr);
         myStrcat(output, "] ");
-        delayMilliseconds(1);
-
-        y1 = readAccelerometerData(Y_HIGH_AXIS_REGISTER) << 8;
-        delayMilliseconds(1);
-        y2 = readAccelerometerData(Y_LOW_AXIS_REGISTER);
-        y = (int16_t)((y1 | y2) >> 5);
-        delayMilliseconds(1);
         intToStr(y, numStr);
         myStrcat(output, "[Y: ");
         myStrcat(output, numStr);
         myStrcat(output, "] ");
-        delayMilliseconds(1);
-
-        z1 = readAccelerometerData(Z_HIGH_AXIS_REGISTER) << 8;
-        delayMilliseconds(1);
-        z2 = readAccelerometerData(Z_LOW_AXIS_REGISTER);
-        z = (int16_t)((z1 | z2) >> 5);
-        delayMilliseconds(1);
         intToStr(z, numStr);
         myStrcat(output, "[Z: ");
         myStrcat(output, numStr);
         myStrcat(output, "]\r\n");
-        delayMilliseconds(1);
 
         bitBangSerial(output, DELAY_11520_BAUD);
         delayMilliseconds(200);  // 200 ms delay
@@ -289,14 +272,13 @@ void showAccelerometerSample()
 #ifdef _DEBUG
 
 #define STRING_LITERAL "1:1 In the beginning God created the heaven and the earth.\r\n1:2 And the earth was without form, and void; and darkness was upon\r\nthe face of the deep. And the Spirit of God moved upon the face of the\r\n\r\nwaters.\r\n\r\n1:3 And God said, Let there be light: and there was light.\r\n\r\n1:4 And God saw the light, that it was good: and God divided the light\r\nfrom the darkness.\r\n\r\n1:5 And God called the light Day, and the darkness he called Night.\r\nAnd the evening and the morning were the first day.\r\n\r\n1:6 And God said, Let there be a firmament in the midst of the waters,\r\nand let it divide the waters from the waters.\r\n\r\n1:7 And God made the firmament, and divided the waters which were\r\nunder the firmament from the waters which were above the firmament:\r\nand it was so.\r\n\r\n1:8 And God called the firmament Heaven. And the evening and the\r\nmorning were the second day.\r\n\r\n1:9 And God said, Let the waters under the heaven be gathered together\r\nunto one place, and let the dry land appear: and it was so.\r\n\r\n1:10 And God called the dry land Earth; and the gathering together of\r\nthe waters called he Seas: and God saw that it was good.\r\n\r\n1:11 And God said, Let the earth bring forth grass, the herb yielding\r\nseed, and the fruit tree yielding fruit after his kind, whose seed is\r\nin itself, upon the earth: and it was so.\r\n\r\n1:12 And the earth brought forth grass, and herb yielding seed after\r\nhis kind, and the tree yielding fruit, whose seed was in itself, after\r\nhis kind: and God saw that it was good.\r\n\r\n1:13 And the evening and the morning were the third day.\r\n\r\n1:14 And God said, Let there be lights in the firmament of the heaven\r\nto divide the day from the night; and let them be for signs, and for\r\nseasons, and for days, and years: 1:15 And let them be for lights in\r\nthe firmament of the heaven to give light upon the earth: and it was\r\nso.\r\n\r\n1:16 And God made two great lights; the greater light to rule the day,\r\nand the lesser light to rule the night: he made the stars also.\r\n\r\n1:17 And God set them in the firmament of the heaven to give light\r\nupon the earth, 1:18 And to rule over the day and over the night, and\r\nto divide the light from the darkness: and God saw that it was good.\r\n\r\n1:19 And the evening and the morning were the fourth day.\r\n\r\n1:20 And God said, Let the waters bring forth abundantly the moving\r\ncreature that hath life, and fowl that may fly above the earth in the\r\nopen firmament of heaven.\r\n\r\n1:21 And God created great whales, and every living creature that\r\nmoveth, which the waters brought forth abundantly, after their kind,\r\nand every winged fowl after his kind: and God saw that it was good.\r\n\r\n1:22 And God blessed them, saying, Be fruitful, and multiply, and fill\r\nthe waters in the seas, and let fowl multiply in the earth.\r\n\r\n1:23 And the evening and the morning were the fifth day.\r\n\r\n1:24 And God said, Let the earth bring forth the living creature after\r\nhis kind, cattle, and creeping thing, and beast of the earth after his\r\nkind: and it was so.\r\n\r\n1:25 And God made the beast of the earth after his kind, and cattle\r\nafter their kind, and every thing that creepeth upon the earth after\r\nhis kind: and God saw that it was good.\r\n\r\n1:26 And God said, Let us make man in our image, after our likeness:\r\nand let them have dominion over the fish of the sea, and over the fowl\r\nof the air, and over the cattle, and over all the earth, and over\r\nevery creeping thing that creepeth upon the earth.\r\n\r\n1:27 So God created man in his own image, in the image of God created\r\nhe him; male and female created he them.\r\n\r\n1:28 And God blessed them, and God said unto them, Be fruitful, and\r\nmultiply, and replenish the earth, and subdue it: and have dominion\r\nover the fish of the sea, and over the fowl of the air, and over every\r\nliving thing that moveth upon the earth.\r\n\r\n1:29 And God said, Behold, I have given you every herb bearing seed,\r\nwhich is upon the face of all the earth, and every tree, in the which\r\nis the fruit of a tree yielding seed; to you it shall be for meat.\r\n\r\n1:30 And to every beast of the earth, and to every fowl of the air,\r\nand to every thing that creepeth upon the earth, wherein there is\r\nlife, I have given every green herb for meat: and it was so.\r\n\r\n1:31 And God saw every thing that he had made, and, behold, it was\r\nvery good. And the evening and the morning were the sixth day."
+#define SMALL_STRING "findPatterns :: String -> [String] -> [[Int]]; findPatterns data' patterns = [ [idx | idx <- [1..length data' - length p + 1], p == take (length p) (drop (idx - 1) data')] | p <- patterns]"
 #define STRING_SIZE sizeof(STRING_LITERAL)
 
 int main()
 {
     char string[STRING_SIZE];
     strncpy(string, STRING_LITERAL, STRING_SIZE);
-    initGPIO();
-    configureI2C();
 
     // bitBangSerial(string, DELAY_11520_BAUD);
     showAccelerometerSample();
