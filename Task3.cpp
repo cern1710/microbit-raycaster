f77d761bcb10d663be0f0906cfa0d65688fc63b6
Samuel Wu <cernunnos1710@gmail.com>
2023-11-21 09:18:23 +0000

    commit

--- Task3.cpp
+++ Task3.cpp
@@ -1,11 +1,11 @@
 #include "Microbit.h"
 #include "nrf52833.h"
 
-// #define _DEBUG
+#define _DEBUG
 
 #define SCL_PIN         8
 #define SDA_PIN         16
-#define SERIAL_TX_PIN   6
+#define SERIAL_TX_PIN   2
 #define PIN_CNF_S0D1    0x00000600
 
 #define TWI_DISABLE         0
@@ -308,7 +308,7 @@ int main()
 
     while(1)
         bitBangSerial(small);
-    showAccelerometerSample();
+    // showAccelerometerSample();
 }
 
 #endif
